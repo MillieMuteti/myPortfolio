ddc582093e2831bd3efdd9e7db5f2a6207e695d4
Millie Muteti <milliemuteti@gmail.com>
2023-06-27 11:15:33 +0300

    update the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Header from "./components/header/Header"
-
+import Nav from "./components/nav/Nav"
+import About from "./components/about/About"
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio';
+import Experience from './components/experience/Experience';
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>App here</h1>
+        <Header />
+        <Nav />
+        <About />
+        <Services />
+        <Portfolio />
+        <Experience />
+        <Testimonials />
+        <Contact />
+        <Footer />
     </div>
   )
 }
