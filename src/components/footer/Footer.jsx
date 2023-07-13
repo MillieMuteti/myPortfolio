73783023eabedc953dde21ec12adc4b5a04a9939
Millie Muteti <milliemuteti@gmail.com>
2023-07-13 14:40:29 +0300

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,23 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './footer.css'; 
 import {FaTwitter} from 'react-icons/fa'
 import  {FaLinkedin} from 'react-icons/fa'
 import {FaGithub} from 'react-icons/fa'
 
+
 const Footer = () => {
   return (
     <footer>
       <div className="footer-content">
         <div className="footer-text">
-          <p>&copy; 2023 Your Website. All rights reserved.</p>
+          <p>&copy; 2023 Millie Muteti. All rights reserved.</p>
         </div>
         <div className="footer-social">
           <a href="https://www.twitter.com"><FaTwitter /></a>
           <a href="https://www.linkedin.com"><FaLinkedin /></a>
           <a href="https://www.github.com"><FaGithub /></a>
+          
         </div>
       </div>
     </footer>
