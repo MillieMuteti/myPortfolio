ae0a3521f7bcad25e0fd6055e23507b4a61cbb4f
Millie Muteti <milliemuteti@gmail.com>
2023-07-13 14:21:20 +0300

    add testimonials section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,13 +1,29 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import "./testimonials.css"
+import React from 'react';
+import './testimonials.css'; 
+import avatar from "../../assets/avatar1.jpg"
 
-const Testimonials = () => {
+const Testimonial = () => {
   return (
-    <section id='testimonials'>
-      <h1>Testimonials</h1>
+    <section id="testimonial">
+      <div className="testimonial-content">
+        <h2>Testimonial</h2>
+        <div className="testimonial">
+          <div className="testimonial-text">
+            <p>
+              <q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat.</q>
+            </p>
+            <p>- John Doe</p>
+          </div>
+          <div className="testimonial-image">
+            <img src={avatar} alt="Testimonial" />
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonial;
