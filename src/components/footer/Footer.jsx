93afd93bf1bf43f1c910072b1dffe3d3a83acc34
Millie Muteti <milliemuteti@gmail.com>
2023-07-13 13:35:47 +0300

    add socials on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './footer.css'; 
+import {FaTwitter} from 'react-icons/fa'
+import  {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -9,15 +12,9 @@ const Footer = () => {
           <p>&copy; 2023 Your Website. All rights reserved.</p>
         </div>
         <div className="footer-social">
-          <a href="https://www.twitter.com">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://www.linkedin.com">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="https://www.github.com">
-            <i className="fab fa-github"></i>
-          </a>
+          <a href="https://www.twitter.com"><FaTwitter /></a>
+          <a href="https://www.linkedin.com"><FaLinkedin /></a>
+          <a href="https://www.github.com"><FaGithub /></a>
         </div>
       </div>
     </footer>
