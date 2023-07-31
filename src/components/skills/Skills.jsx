256f1a0883877119c7f42dcbd5b26580d03d7adf
Millie Muteti <milliemuteti@gmail.com>
2023-07-31 16:48:53 +0300

    work on the technical skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -15,8 +15,70 @@ function Skills() {
                             <div className='skill'>
                                 <div className='partial'>
                                     <div className='info'>
-                                    
+                                        <div className='name'>JavaScript</div>
+
+                                        <div className='percentage'>90%</div>
+
                                     </div>
+                                    <div className='progression-bar'>
+
+                                        <div className='percentagem' style={{width:"90%"}}></div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            </div>
+
+                            <div className='each-skill'>
+                            <div className='skill'>
+                                <div className='partial'>
+                                    <div className='info'>
+                                        <div className='name'>React</div>
+
+                                        <div className='percentage'>75%</div>
+
+                                    </div>
+                                    <div className='progression-bar'>
+
+                                        <div className='percentagem' style={{width:"75%"}}></div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            </div>
+
+                            <div className='each-skill'>
+                            <div className='skill'>
+                                <div className='partial'>
+                                    <div className='info'>
+                                        <div className='name'>CSS</div>
+
+                                        <div className='percentage'>87%</div>
+
+                                    </div>
+                                    <div className='progression-bar'>
+
+                                        <div className='percentagem' style={{width:"87%"}}></div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            </div>
+
+                            <div className='each-skill'>
+                            <div className='skill'>
+                                <div className='partial'>
+                                    <div className='info'>
+                                        <div className='name'>PhP</div>
+
+                                        <div className='percentage'>40%</div>
+
+                                    </div>
+                                    <div className='progression-bar'>
+
+                                        <div className='percentagem' style={{width:"40%"}}></div>
+                                    </div>
+
                                 </div>
                             </div>
                             </div>
@@ -24,7 +86,16 @@ function Skills() {
                         </div>
                     </div>
                 </div>
-                <div className='col-md-6 col-sm-12'></div>
+                <div className='col-md-6 col-sm-12'>
+                    <div className='professional-skills'>
+                        <h3>Professional Skills</h3>
+                        <ul className='professional-list'>
+                            <li>
+                                <div className='progress progress-circle'></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
             </div>
         </div>
