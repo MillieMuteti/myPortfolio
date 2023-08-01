556aa435d0f8860e953c3928649ffd11db249c0a
Millie Muteti <milliemuteti@gmail.com>
2023-08-01 13:51:47 +0300

    add progress bar

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,105 +1,66 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import ProgressBar from "@ramonak/react-progress-bar";
+import CircularProgress from '@mui/joy/CircularProgress';
+
+
 import './skills.css'
+
+
+
 function Skills() {
+
   return (
-    <section id='skills'>
-        <div className='container skills-content'>
-        <h2>Skills</h2>
-            <div className='row'>
-                <div className='col-md-6 col-sm-12'>
-                    <div>
-                        <div className='professional-skills'>
-                            <h3>Technical Skills</h3>
-                            <div className='each-skill'>
-                            <div className='skill'>
-                                <div className='partial'>
-                                    <div className='info'>
-                                        <div className='name'>JavaScript</div>
-
-                                        <div className='percentage'>90%</div>
-
-                                    </div>
-                                    <div className='progression-bar'>
-
-                                        <div className='percentagem' style={{width:"90%"}}></div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            </div>
-
-                            <div className='each-skill'>
-                            <div className='skill'>
-                                <div className='partial'>
-                                    <div className='info'>
-                                        <div className='name'>React</div>
-
-                                        <div className='percentage'>75%</div>
-
-                                    </div>
-                                    <div className='progression-bar'>
-
-                                        <div className='percentagem' style={{width:"75%"}}></div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            </div>
-
-                            <div className='each-skill'>
-                            <div className='skill'>
-                                <div className='partial'>
-                                    <div className='info'>
-                                        <div className='name'>CSS</div>
-
-                                        <div className='percentage'>87%</div>
-
-                                    </div>
-                                    <div className='progression-bar'>
-
-                                        <div className='percentagem' style={{width:"87%"}}></div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            </div>
-
-                            <div className='each-skill'>
-                            <div className='skill'>
-                                <div className='partial'>
-                                    <div className='info'>
-                                        <div className='name'>PhP</div>
-
-                                        <div className='percentage'>40%</div>
-
-                                    </div>
-                                    <div className='progression-bar'>
-
-                                        <div className='percentagem' style={{width:"40%"}}></div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            </div>
-                          
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-6 col-sm-12'>
-                    <div className='professional-skills'>
-                        <h3>Professional Skills</h3>
-                        <ul className='professional-list'>
+   <section id='skills'>
+        <div className='container skill-content'>
+            <h2>Skills</h2>
+            <Container fluid>
+                <Row>
+                    <Col xs={12} md={6} debug>
+                        <h3>Programming Languages</h3>
+                        <ul>
                             <li>
-                                <div className='progress progress-circle'></div>
+                             <h4>JavaScript/TypeScript</h4>
+                             <ProgressBar completed = {85} bgColor = "var( --color-primary)" animateOnRender = {true} />
+
+                            </li>
+
+                            <li>
+                            <h4>CSS/SASS/Bootstrap</h4>
+                            <ProgressBar completed = {90} bgColor = "var( --color-primary)" animateOnRender = {true} />
+                           </li>
+
+                           <li>
+                           <h4>React</h4>
+                           <ProgressBar completed = {74} bgColor = "var( --color-primary)" animateOnRender = {true} />
+
+                          </li>
+
+                          <li>
+                          <h4>PHP</h4>
+                          <ProgressBar completed = {33.5} bgColor = "var( --color-primary)" animateOnRender = {true} />
+
+                         </li>
+                        </ul>
+                    
+                    </Col>
+                    <Col xs={12} md={6} debug>
+                        <h3>Professional Skills</h3>
+                        <ul>
+                            <li>
+                                <h4>Communication</h4>
+                              Excellent communication skills, both written and oral.
+                        
                             </li>
                         </ul>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
+            </Container>
 
-            </div>
+           
         </div>
-    </section>
+   </section>
   )
 }
 
