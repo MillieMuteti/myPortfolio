4d520a2d7b7f90028564a8da3fda8dee9a37a2a9
Millie Muteti <milliemuteti@gmail.com>
2023-09-18 21:58:08 +0300

    update experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import './Experience.css'; 
+import './experience.css'; 
 
 const Experience = () => {
   const experiences = [
